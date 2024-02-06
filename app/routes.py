21fd57f7650353a5400be28529d550988a403a6b
Faris Imran <farisimran.faisal@gmail.com>
2024-02-06 16:01:15 +0800

    Added board (discussion board) page

--- app/routes.py
+++ app/routes.py
@@ -2,8 +2,17 @@ from app import app
 from flask import render_template
 
 @app.route('/')
+
+
+
 @app.route('/index')
 def index():
+    user = {'username': 'Faris'}
+
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/board')
+def board():
     user = {'username': 'Faris'}
     posts = [
         {
@@ -15,4 +24,8 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+
+    return render_template('board.html',
+                            title='Discussion Board',
+                              user=user,
+                                posts=posts)
